27615860f6615844fdbbe0019453946166168b40
大谷 凌也 <>
2022-03-17 19:09:40 +0900

    kvのフォント変えました

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
         <p>Copyright &copy; RIGHTS RESERVED.</p>
-    </footer>
+</footer>
     <?php wp_footer();?>
     
     <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
